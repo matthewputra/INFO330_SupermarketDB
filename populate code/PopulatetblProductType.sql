a338ecbb5b8ad00407780209e7f6621aa4014ff2
yiu chung <59578307+yiuchungcheung@users.noreply.github.com>
2020-05-30 23:02:38 -0700

    Add files via upload

---
+++ populate code/PopulatetblProductType.sql
@@ -0,0 +1,14 @@
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Vegetable', 'veggies');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Fruit', 'fruits');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Chips', 'Snacks');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Beverage', 'Drinks');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Vegetable', 'veggies');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Alcohol', 'Drinks');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Tobacco', '18+');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Medicine', 'Drugs');
+
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Dairy  & Eggs', 'Milk');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Frozen Foods', 'Refrigerated');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Meat', 'Animals');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Cleaning Supplies', 'Cleaning Products');
+INSERT INTO tblProductType (ProductTypeName, ProductTypeDescription) values ('Pet Care', 'Pet Stuff');
