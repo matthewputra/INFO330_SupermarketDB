c4ab4f4c356baaa23cc43aecb59d24b83095e3a1
yiu chung <59578307+yiuchungcheung@users.noreply.github.com>
2020-06-03 18:32:59 -0700

    Add files via upload

---
+++ populate code/tblemployeedepartment.sql
@@ -0,0 +1,6 @@
+USE INFO330_PROJ_A13
+insert into tblemployeedepartment (EmployeeID, DepartmentID, PositionID, EmployeeDepartmentStartDate, EmployeeDepartmentEndDate) values (1, 3.09, 6, '12/15/2000', '2/1/2020');
+insert into tblemployeedepartment (EmployeeID, DepartmentID, PositionID, EmployeeDepartmentStartDate, EmployeeDepartmentEndDate) values (2, 6.73, 3, '7/18/2000', '4/25/2020');
+insert into tblemployeedepartment (EmployeeID, DepartmentID, PositionID, EmployeeDepartmentStartDate, EmployeeDepartmentEndDate) values (3, 6.26, 6, '5/30/2001', '4/8/2020');
+insert into tblemployeedepartment (EmployeeID, DepartmentID, PositionID, EmployeeDepartmentStartDate, EmployeeDepartmentEndDate) values (4, 3.72, 3, '6/14/2000', '2/18/2020');
+insert into tblemployeedepartment (EmployeeID, DepartmentID, PositionID, EmployeeDepartmentStartDate, EmployeeDepartmentEndDate) values (5, 1.32, 6, '6/21/2000', '7/21/2019');
