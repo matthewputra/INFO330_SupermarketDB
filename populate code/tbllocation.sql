9cdc3a4a1f38a821339cb497e95135eb87deccf0
yiu chung <59578307+yiuchungcheung@users.noreply.github.com>
2020-06-03 18:17:06 -0700

    Add files via upload

---
+++ populate code/tbllocation.sql
@@ -0,0 +1,7 @@
+use INFO330_PROJ_A13
+
+insert into tblLocation (LocationName) values ('Downtown Seattle');
+insert into tblLocation (LocationName) values ('International District');
+insert into tblLocation (LocationName) values ('Kent');
+insert into tblLocation (LocationName) values ('Bellevue');
+insert into tblLocation (LocationName) values ('Renton');
