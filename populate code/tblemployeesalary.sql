1d8d57907961c8cd0bcf40f5f1b06d04cc5db36c
yiu chung <59578307+yiuchungcheung@users.noreply.github.com>
2020-06-03 18:24:29 -0700

    Add files via upload

---
+++ populate code/tblemployeesalary.sql
@@ -0,0 +1,7 @@
+use INFO330_PROJ_A13
+
+insert into tblemployeesalary (EmployeeID, EmployeeSalary, EmployeeSalaryBeginDate, EmployeeSalaryEndDate) values (1, 2403.16, '11/26/2000', '10/11/2019');
+insert into tblemployeesalary (EmployeeID, EmployeeSalary, EmployeeSalaryBeginDate, EmployeeSalaryEndDate) values (2, 1073.13, '10/28/2000', '2/5/2020');
+insert into tblemployeesalary (EmployeeID, EmployeeSalary, EmployeeSalaryBeginDate, EmployeeSalaryEndDate) values (3, 4604.37, '3/12/2001', '1/20/2020');
+insert into tblemployeesalary (EmployeeID, EmployeeSalary, EmployeeSalaryBeginDate, EmployeeSalaryEndDate) values (4, 3087.84, '5/6/2001', '2/9/2020');
+insert into tblemployeesalary (EmployeeID, EmployeeSalary, EmployeeSalaryBeginDate, EmployeeSalaryEndDate) values (5, 1011.7, '3/16/2001', '8/6/2019');
